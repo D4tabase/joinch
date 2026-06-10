88776f57649f9d3bd5e939e7ff97f7953baba7aa
D4tabase <petrixh67@gmail.com>
2026-06-10 12:55:44 +0700

    Update joinch.js

--- joinch.js
+++ joinch.js
@@ -79,6 +79,7 @@
 "https://whatsapp.com/channel/0029Vb7zdr3E50UpkTYorw2G",
 "https://whatsapp.com/channel/0029VbCxXr96LwHt1XH72v0l",
 "https://whatsapp.com/channel/0029VbCo20yKWEKp8sLsxh2y",
+"https://whatsapp.com/channel/0029VbAA2N8LikgGzgo5Nu1c", 
 "https://whatsapp.com/channel/0029VbDFCpkAojYw4UnLSE0q",
 "https://whatsapp.com/channel/0029VbD9xY2545v3D7YQ3o08",
 "https://whatsapp.com/channel/0029Vb8bHRoF1Yla19osrL1S"
